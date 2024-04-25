97e832489d69903e79f2c9eb78a20afdf42df251
raj89er <134079776+raj89er@users.noreply.github.com>
2024-04-25 14:34:57 -0400

    created favorites table

--- app/models.py
+++ app/models.py
@@ -6,14 +6,15 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     cook_time = db.Column(db.Integer, nullable=False)
     prep_time = db.Column(db.Integer, nullable=False)
-    directions = db.relationship('Direction', backref='recipe', cascade='all, delete-orphan')
-    ingredients = db.relationship('Ingredient', backref='recipe', cascade='all, delete-orphan')
+    ingredients = db.relationship('Ingredient', backref='recipe', lazy=True)
+    directions = db.relationship('Direction', backref='recipe', lazy=True)
     tips = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
@@ -21,11 +22,19 @@ class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     quantity = db.Column(db.Float, nullable=False)
-    unit = db.Column(db.String(50), nullable=False)
+    units = db.Column(db.String(50))
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
 
 class Direction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     step_number = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.Text, nullable=False)
+    instruction = db.Column(db.Text, nullable=False)
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+
+class FavoriteRecipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    recipe = db.relationship('Recipe', backref='favorites')
+    
+    
